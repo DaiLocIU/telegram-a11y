21b029c1fbb37797fa45c14f5bd1396eac6c98ab
DaiLoc <loc.ldl.itou@gmail.com>
2025-05-25 20:02:59 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template lang="pug">
+  div.text-3xl.font-bold.text-center.pt-10
+    a(href="https://vite.dev", target="_blank")
+      img.logo(src="/vite.svg", alt="Vite logo")
+    a(href="https://vuejs.org/", target="_blank")
+      img.logo.vue(src="./assets/vue.svg", alt="Vue logo")
+  HelloWorld(msg="Vite + Vue")
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;  
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
