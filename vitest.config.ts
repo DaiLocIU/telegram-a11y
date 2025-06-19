02a785f06ab6662a04ac15cea73be49a8d7495cf
DaiLoc <loc.ldl.itou@gmail.com>
2025-06-19 15:39:00 +0700

    refactor: simplify vitest configuration by consolidating test settings

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vitest/config'
+
+import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import tailwindcss from '@tailwindcss/vite'
 import pugPlugin from "vite-plugin-pug"
 
 export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+    browser: {
+        enabled: true,
+        instances: [
+          { browser: 'chromium' },
+        ],
+      },
+    include: ['**/*.test.browser.ts', '**/*.test.ts'],
+    pool: "vmThreads",
+    css: false,
+  },
   plugins: [
     vue(),
     vuetify({
@@ -18,33 +32,4 @@ export default defineConfig({
       '@': '/src',
     },
   },
-  test: {
-    projects: [
-      {
-        test: {
-          environment: 'jsdom',
-          // an example of file based convention,
-          // you don't have to follow it
-          setupFiles: ['./vitest.setup.ts'],
-          include: ['**/*.test.ts', '**/*.test.tsx'],
-          name: 'unit',
-          css: false,
-        },
-      },
-      {
-        test: {
-          environment: 'jsdom',
-          setupFiles: ['./vitest.setup.ts'],
-          include: ['**/*.test.browser.ts', '**/*.test.browser.tsx'],
-          name: 'browser',
-          browser: {
-            enabled: true,
-            instances: [
-              { browser: 'chromium' },
-            ],
-          },
-        },
-      },
-    ],
-  }
-})
+});
